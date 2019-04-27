06c79fc5e05dc29029f8f6047cd5cabc56b627db
tobi <tobiasschmitz2001@gmail.com>
2019-04-28 00:54:58 +0200

    added tablayout with left and right webview

--- main/java/com/tnt/vertretungsplan/MainActivity.kt
+++ main/java/com/tnt/vertretungsplan/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.tnt.vertretungsplan
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var mywebview: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mywebview = findViewById<WebView>(R.id.webview)
-        mywebview!!.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url)
-                return true
-            }
-        }
-        mywebview!!.loadUrl("https://www.bgh-windeck.de/cunst/Schueler/left/subst_001.htm")
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdapter
+
+        tabs_main.setupWithViewPager(viewpager_main)
     }
 }
