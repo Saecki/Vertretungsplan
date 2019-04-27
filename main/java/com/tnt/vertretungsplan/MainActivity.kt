4c0081d19f74750d4c30071aefc9198f33e3dd75
tobi <tobiasschmitz2001@gmail.com>
2019-04-27 21:30:47 +0200

    not the yellow from the egg but it goes

--- main/java/com/tnt/vertretungsplan/MainActivity.kt
+++ main/java/com/tnt/vertretungsplan/MainActivity.kt
@@ -2,11 +2,22 @@ package com.tnt.vertretungsplan
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
+    var mywebview: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mywebview = findViewById<WebView>(R.id.webview)
+        mywebview!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        mywebview!!.loadUrl("https://www.bgh-windeck.de/cunst/Schueler/left/subst_001.htm")
     }
 }
